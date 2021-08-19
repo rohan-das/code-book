44f76ebb9aa7813fc2b6d2d884aa7096a0691637
Rohan Das <hello.rd24@gmail.com>
2021-08-19 20:20:07 +0530

    Leetcode - Majority Element

---
+++ Arrays/majority_element.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/majority-element/
+
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int n = nums.size();
+		int ans = nums[0], count = 1;
+
+		for (int i = 1; i < n; i++) {
+
+			if (nums[i] == ans) count++;
+			else count--;
+
+			if (count == 0) {
+				ans = nums[i];
+				count = 1;
+			}
+		}
+
+		return ans;
+	}
+};
